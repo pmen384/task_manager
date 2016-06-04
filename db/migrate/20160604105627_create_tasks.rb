3bb06f01c7a0d565f39db2dd0b068427d6171199
takahiro sahashi <pmen384@gmail.com>
2016-06-04 11:28:47 +0000

    before model add.

---
+++ db/migrate/20160604105627_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.references :user, index: true, foreign_key: true
+      t.string :title
+      t.text :content
+      t.boolean :do, default: false
+
+      t.timestamps null: false
+      t.index [:user_id, :created_at]
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604075029) do
+ActiveRecord::Schema.define(version: 20160604105627) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.boolean  "do",         default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
